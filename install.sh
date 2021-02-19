019034744225f1910ab5d34345deb9076023da47
jasongerhardt1903 <34262377+jasongerhardt1903@users.noreply.github.com>
2021-02-19 17:33:29 +0800

    Update install.sh
    fixed bugs of setting a new root directory

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 sudo apt-get install apache2 -y 
 # add config file
 sudo git clone https://github.com/jasongerhardt1903/testconfig /tmp/config/
-sudo mv /etc/apache/apache2.conf /etc/apache/apache2.conf.`date “+%Y%m%d-%H%M”`
-sudo mv /tmp/config/apache2.conf /etc/apache/apache2.conf
+sudo mv /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/000-default.conf.`date "+%Y%m%d-%H%M"`
+sudo mv /tmp/config/000-default.conf /etc/apache2/sites-available/000-default.conf
 # add web page
-sudo get clone https://github.com/jasongerhardt1903/testwebpage /tmp/webpage
+sudo git clone https://github.com/jasongerhardt1903/testwebpage /tmp/webpage
 sudo mv /tmp/webpage /var/www2
 # restart apache2 
 sudo systemctl restart apache2
