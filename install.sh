189d3e5c3c9cc30e5bf333718f7ba87ed166ae53
jasongerhardt1903 <34262377+jasongerhardt1903@users.noreply.github.com>
2021-02-19 17:38:59 +0800

    Update install.sh
    add marks

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 # install apach2
+echo ================================= install apache2 ====================================
 sudo apt-get install apache2 -y 
 # add config file
+echo ================================= start setup config files ================================
 sudo git clone https://github.com/jasongerhardt1903/testconfig /tmp/config/
 sudo mv /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/000-default.conf.`date "+%Y%m%d-%H%M"`
 sudo mv /tmp/config/000-default.conf /etc/apache2/sites-available/000-default.conf
 # add web page
+echo ================================== clone webpages ==========================================
 sudo git clone https://github.com/jasongerhardt1903/testwebpage /tmp/webpage
 sudo mv /tmp/webpage /var/www2
 # restart apache2 
+echo ================================== restart apache2 ==================================
 sudo systemctl restart apache2
